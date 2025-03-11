9a6a525c05e2f8dc0feb0a4ff2bf73c82b1fdd75
Pedro Silva <pedro@dimensiva.com.br>
2025-03-11 09:56:35 -0300

    Configurado o service default para implementação dos demais services

---
+++ src/app/shared/services/Base.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { BehaviorSubject, Observable, catchError, throwError, take, tap } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const BASE_URL = environment.api;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+
+  protected http: HttpClient = inject(HttpClient);
+
+  protected get(url: string, eventComponent: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false), msgError?: string, build?: any, sort?: string): Observable<HttpResponse<any>> {
+    eventComponent?.next(true);
+    const sortParam = sort ? `&${sort}` : '';
+    return this.http.get<any>(`${BASE_URL}${url}?success=true${this._getInfoBuild(build)}${sortParam}`, { observe: 'response' }).pipe(catchError(
+    (err: any) => {
+        eventComponent?.next(false);
+        return throwError(() => err);
+      }
+    )).pipe(take(1)).pipe(tap((_: any) => eventComponent?.next(false)));
+  }
+
+  /******************* METHODS PRIVATE *******************/
+
+  private _getInfoBuild(paramsBuild: any): string {
+    const build = this._getParamsBuild(paramsBuild);
+    const arrayActive = build?.filter(r => r);
+    const arrayNormalized = arrayActive?.map(r => {
+      const params = r.split('=');
+      const data = params[1]?.replaceAll(',', '@');
+      return `${params[0]}=${data}`;
+    })
+    const params = arrayNormalized?.toString()?.replaceAll(',', '')?.replaceAll('@', ',')
+    return params;
+  }
+
+  private _getParamsBuild(build: any): string[] {
+    let array: string[] = [];
+    if (build) {
+      Object.keys(build)?.forEach(key => {
+        const isFieldValid = ((Array.isArray(build[key]) && build[key]?.length) || !Array.isArray(build[key])) ? true : false;
+        if (build[key] != null && build[key] != undefined && isFieldValid) {
+          const item = `&${key}=${encodeURIComponent(build[key])}`;
+          if(item && item?.length) {
+            array.push(item);
+          }
+        }
+      });
+    }
+    return array;
+  }
+
+}
