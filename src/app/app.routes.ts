8e5fa27cfd71451cc5da3618ac13c7c957a15cd4
Pedro Silva <pedro@dimensiva.com.br>
2025-03-11 21:23:43 -0300

    Ajustado a rota de erro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CreditoComponent } from './pages/credito/credito.component';
 import { ROUTE } from './shared/enums/route.enum';
 
 export const routes: Routes = [
+  {path: '**', redirectTo: ROUTE.CREDITO, pathMatch: 'full'},
   {path: '', redirectTo: ROUTE.CREDITO, pathMatch: 'full'},
   {path: ROUTE.CREDITO, component: CreditoComponent},
 ];
