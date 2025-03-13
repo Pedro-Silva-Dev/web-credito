ec3577633baaf07bb875c85c8b61009dafad8260
Pedro Silva <pedro@dimensiva.com.br>
2025-03-12 22:16:40 -0300

    Adicionado a configuração do docker

---
+++ start.sh
@@ -0,0 +1,6 @@
+# remover o container docker.
+docker-compose down
+# buildar e executar todos os comandos necessários para deixar o container pronto para uso.
+docker-compose build
+# subir o container da aplicação web front-end.
+docker-compose up -d
